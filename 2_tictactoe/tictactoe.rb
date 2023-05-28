7e1dab253b8b3a1ad5971bb1011a2c4219c03e5e
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-05-28 18:55:03 +0300

    add two players and legality of turns

--- 2_tictactoe/tictactoe.rb
+++ 2_tictactoe/tictactoe.rb
@@ -1,8 +1,10 @@
 class Board
   def initialize(rows, columns)
     if rows != 3 || columns != 3
-      p "ERROR: GO WITH 3x3 FOR NOW"
+      p 'ERROR: GO WITH 3x3 FOR NOW'
     else
+      @x_is_legal = true 
+      @o_is_legal = true
       @rows = rows
       @columns = columns
       @board = Hash.new(0)
@@ -13,68 +15,95 @@ class Board
         array = array.map do | number |
           number.to_s
         end
-        puts array
         @board[i] = array
         i += 1
         i_d -= 1
       end
     @board
+    display()
     end
-  end
-
-  def change_board(number, symbol)
-    row = (number / @columns)
-    if number % @columns != 0
-      row += 1
-    end
-    p row
-    column = ((@columns * ((number.to_f / @columns) - (row - 1))) - 1).round(0)
-    @board[row][column] = symbol
   end
 
   def display
+    p "-------------"
     i = 1
     @rows.times do
       p @board[i]
       i += 1
     end
+    p "-------------"
+  end
+
+  def change_board(number, symbol)
+    get_coordinates(number, symbol)
+    if legal?(symbol)
+      process_turn(symbol)
+      set_legality(symbol)
+    else
+      p "ERROR: ILLEGAL TURN"
+    end
+    display()
+  end
+
+  private 
+  def get_coordinates(number, symbol)
+    @row = (number / @columns)
+    if number % @columns != 0
+      @row += 1
+    end
+    @column = ((@columns * ((number.to_f / @columns) - (@row - 1))) - 1).round(0)
+  end
+
+  def process_turn(symbol)
+    @board[@row][@column] = symbol
+  end
+
+
+  def legal?(symbol)
+    if @board[@row][@column] == "X" || @board[@row][@column] == "O"
+      false
+    elsif symbol == "X" && @x_is_legal == true
+      true
+    elsif symbol == "O" && @o_is_legal == true
+      true
+    else
+      false
+    end
+  end
+
+  def set_legality(symbol)
+    if symbol == "X"
+      @x_is_legal = false
+      @o_is_legal = true 
+    elsif symbol == "O"
+      @x_is_legal = true
+      @o_is_legal = false 
+    end
   end
 end
 
-module Playable
+class Player
+  def initialize(board_name, symbol)
+    if symbol != "X" && symbol != "O"
+      p "ERROR: WRONG SYMBOL"
+    else  
+      @board_name = board_name
+      @symbol = symbol
+    end
+  end
+
   def play(number)
     @board_name.change_board(number, @symbol)
   end
 end
 
 
-class PlayerOne
-  include Playable
-
-  def initialize(board_name)
-    @board_name = board_name
-    @symbol = "X"
-  end
-end
-
-class PlayerTwo
-  include Playable
-  
-  def initialize(name, board_name)
-    @name = name
-    @name = board_name
-    @symbol = "O"
-  end
-end
-
 
 threeBoard = Board.new(3, 3)
-threeBoard.display
 
-bob = PlayerOne.new(threeBoard)
-i = 1
+bob = Player.new(threeBoard, "X")
+mag = Player.new(threeBoard, "O")
 
 bob.play(5)
-
-
-threeBoard.display
+mag.play(8)
+bob.play(8)
