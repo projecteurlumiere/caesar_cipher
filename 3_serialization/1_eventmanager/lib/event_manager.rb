20aebb90fb809e8ecf61bdf91ae6c7eb6408e40d
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-06-04 16:13:59 +0300

    event manager popular hours & days of the week exercise

--- 3_serialization/1_eventmanager/lib/event_manager.rb
+++ 3_serialization/1_eventmanager/lib/event_manager.rb
@@ -3,10 +3,13 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
+
+## My code:
 
 def clean_phone(phone)
   phone = phone.delete('().-').delete(' ')
@@ -18,6 +21,26 @@ def clean_phone(phone)
     nil
   end
 end
+
+def get_converted_dates(data, symbol)
+  dates = Array.new(0)
+
+  data.each do |row|
+  date = DateTime.strptime(row[symbol], "%m/%d/%y %H:%M")
+  dates << date
+  end
+
+  dates
+end
+
+def get_popularity(input_array)
+  popularity_array = input_array.reduce(Hash.new(0)) do |thing, procs|
+    thing[procs] += 1
+    thing
+  end
+  popularity_array
+end
+# My code ends
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -56,7 +79,6 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 =begin 
-
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
@@ -66,15 +88,38 @@ contents.each do |row|
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id,form_letter)
-end 
-
+end
 =end
 
 # My code:
 
+# Clean Phone: 
+=begin 
 contents.each do |row|
   name = row[:first_name]
   number = clean_phone(row[:homephone])
 
   puts "#{name} and #{number}"
 end
+=end
+# Getting popular hours:
+
+dates = get_converted_dates(contents, :regdate)
+
+hours = dates.map do |date|
+  date.hour
+end
+
+popular_hours = get_popularity(hours)
+
+p popular_hours.sort_by(&:last).to_h
+
+# Getting popular days of the week:
+
+days_of_the_week = dates.map do |date|
+  date.wday
+end
+
+popular_days = get_popularity(days_of_the_week)
+
+p popular_days.sort_by(&:last).to_h
