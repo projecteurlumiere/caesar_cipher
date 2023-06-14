76d2f5ccd6a6bf16a76bd5cc1ccbb3818156a93c
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-06-14 14:15:26 +0300

    linked list: improve and fix some caveats with empty lists

--- 5_CS/2_linkedlists/linkedlists.rb
+++ 5_CS/2_linkedlists/linkedlists.rb
@@ -3,26 +3,36 @@ class LinkedList
   attr_reader :head
 
   def initialize(*values)
-    values.each do |value|
-      if value == values[0]
-        @head = append(value)
-        @tail = @head
-      else
-        append(value)
+    if values.empty?
+      @head = nil
+      @tail = nil
+    else
+      set_new_list(*values)
+    end
+  end
+
+  def append(*values)
+    if @head.nil?
+      set_new_list(*values)
+    else
+      values.each do |value|
+        @new_tail = Node.new(value)
+        @tail.next_node = @new_tail
+        @tail = @new_tail
       end
     end
   end
 
-  def append(value)
-    @new_tail = Node.new(value)
-    @tail.next_node = @new_tail if @tail != nil
-    @tail = @new_tail
-  end
-
-  def prepend(value)
-    @new_head = Node.new(value)
-    @new_head.next_node = @head.next_node
-    @head = @new_head
+  def prepend(*values)
+    if @head.nil?
+      set_new_list(*values.reverse)
+    else
+      values.each do |value|
+        @new_head = Node.new(value)
+        @new_head.next_node = @head.next_node
+        @head = @new_head
+      end
+    end
   end
 
   def size
@@ -31,7 +41,7 @@ class LinkedList
     @size = 1
     @node = @head.next_node
 
-    until @node == nil
+    until @node.nil?
       @size += 1
       @node = @node.next_node
     end
@@ -40,39 +50,37 @@ class LinkedList
   end
 
   def at(index)
+    return nil if index.negative? || @head.nil?
+
     return @head if index.zero?
 
     @node = @head.next_node
-    i = 1
+    @i = 1
 
-    until index == i
+    until index == @i || @node.nil?
       @node = @node.next_node
-      return nil if @node == nil
-
-      i += 1
+      @i += 1
     end
 
     @node
   end
 
   def pop
-    return nil if @head.nil?
-
-    if @head == @tail
+    if @head.nil?
+      @popped_node_value = nil
+    elsif @head == @tail
       @popped_node_value = @head.value
       @head = nil
-      return @popped_node_value
+      @tail = nil
+    else
+      @node = @head
+
+      @node = @node.next_node until @node.next_node == @tail
+
+      @popped_node_value = @tail.value
+      @node.next_node = nil
+      @tail = @node
     end
-
-    @node = @head
-
-    until @node.next_node == @tail
-      @node = @node.next_node
-    end
-
-    @popped_node_value = @tail.value
-    @node.next_node = nil
-    @tail = @node
 
     @popped_node_value
   end
@@ -83,7 +91,8 @@ class LinkedList
     @node = @head
 
     until @node.value == value
-      return false if @node.next_node == nil
+      return false if @node.next_node.nil?
+
       @node = @node.next_node
     end
 
@@ -91,11 +100,14 @@ class LinkedList
   end
 
   def find(value)
+    return nil if @head.nil?
 
     @index = 0
+
     until value == at(@index).value
-      @index += 1
       break if at(@index).next_node == nil
+
+      @index += 1
     end
 
     return nil unless value == at(@index).value
@@ -104,9 +116,10 @@ class LinkedList
   end
 
   def to_s
-    @node = @head
     @array_of_values = []
-    until @node == nil
+    @node = @head
+
+    until @node.nil?
       @array_of_values << @node.value
       @node = @node.next_node
     end
@@ -114,10 +127,11 @@ class LinkedList
     @string_output = ""
 
     @array_of_values.each do |value|
+      value = 'nil' if value == nil
       @string_output.concat("( #{value} ) -> ")
     end
 
-    @string_output.concat("nil")
+    @string_output.concat('nil')
 
     @string_output
   end
@@ -134,13 +148,25 @@ class LinkedList
   end
 
   def remove_at(index)
-    if index == 0
+    if index.zero?
       @head = @head.next_node
-    elsif at(index) == nil
+    elsif at(index).nil?
       return
     else
       at(index - 1).next_node = at(index + 1)
     end
+  end
+
+  private
+
+  def set_new_list(*values)
+    @head = Node.new(values[0])
+    @tail = @head
+
+    return if values.length <= 1
+
+    values.shift
+    values.each { |value| append(value) }
   end
 end
 
@@ -155,24 +181,34 @@ class Node
 end
 
 list = LinkedList.new(1, 2, 3, 4, 5)
-# p list
-# p list.head
-# p list.tail
-# p list.size
+p list.to_s
+p list.head
+p list.tail
+p list.size
 
-# p list.at(3)
-# p list.pop
-# p list.pop
-# p list.pop
-# p list.size
-# p list
-# p list.contains?(1)
-# p list.contains?(2)
-# p list.contains?(3)
-# p list.find(1)
-list.insert_at(6, 0)
+p list.at(3)
+p list.pop
+p list.pop
+p list.pop
+p list.size
+p list.to_s
+p list.contains?(1)
+p list.contains?(2)
+p list.contains?(3)
+p list.find(1)
+# list.insert_at(6, 0)
 puts list.to_s
-list.insert_at(6, 6)
+# list.insert_at(6, 6)
 puts list.to_s
 list.remove_at(6)
+p list.to_s
+p list.find(5)
+p list.pop
+puts list.to_s
+list.append(1)
+puts list.to_s
+list.append(2)
+puts list.to_s
+list.remove_at(0)
+list.remove_at(0)
 puts list.to_s
